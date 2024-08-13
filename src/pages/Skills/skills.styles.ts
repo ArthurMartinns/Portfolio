687eb28f724c5d16d6ccb271325c4b2dea73adc8
ArthurMartinns <arthurmartiinscontato@gmail.com>
2024-08-13 17:43:58 -0300

    Alterções

---
+++ src/pages/Skills/skills.styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Content = styled.div`
+    width: 100%;
+`
+
+export const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    border: 2px solid #fff;
+`
+
+export const Left = styled.div`
+    width: 100%;
+`
+
+export const Right = styled.div`
+    width: 100%;
+`
--- src/pages/Skills/skills.tsx
+++ src/pages/Skills/skills.tsx
@@ -1,8 +1,20 @@
+import Container from "../../components/Container/container.view"
+import * as S from './skills.styles'
+
 function Skills() {
     return (
-      <>
-      
-      </>
+        <Container>
+          <S.Content>
+            <S.Wrapper>
+              <S.Left>
+
+              </S.Left>
+              <S.Right>
+                
+              </S.Right>
+            </S.Wrapper>
+          </S.Content>
+        </Container>
     )
   }
   
