cdac3d97263629eac505710f75dd50d14bf89f89
ArthurMartinns <arthurmartiinscontato@gmail.com>
2024-08-13 13:21:23 -0300

    Removendo conteúdo

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,15 +5,7 @@ function Home() {
   return (
     <Container>
       <S.Container>
-        <S.Wrapper>
-          <S.Left>
-            <h2>Olá, meu nome é</h2>
-            <h3>Teste</h3>
-          </S.Left>
-          <S.Right>
-            <img src="meHome.png" alt="" />
-          </S.Right>
-        </S.Wrapper>
+        
       </S.Container>
     </Container>
   )
