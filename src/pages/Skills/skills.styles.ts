0926b41e5de1a03a304b36592d8ddab496c130c9
ArthurMartinns <arthurmartiinscontato@gmail.com>
2024-08-20 16:36:42 -0300

    Página de skills feita

--- src/pages/Skills/skills.styles.ts
+++ src/pages/Skills/skills.styles.ts
@@ -2,19 +2,60 @@ import styled from "styled-components";
 
 export const Content = styled.div`
     width: 100%;
+    margin-top: 6rem;
 `
 
 export const Wrapper = styled.div`
-    width: 100%;
     display: flex;
     flex-direction: row;
-    border: 2px solid #fff;
+    justify-content: space-between;
 `
 
 export const Left = styled.div`
     width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+
 `
 
 export const Right = styled.div`
     width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-gap: 2rem;
+    align-self: center;
+`
+
+export const Itens = styled.div`
+    height: 12rem;
+    background-color: #141414;
+    border-radius: 0.8rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: ${({theme}) => theme.COLORS.WHITE};
+
+    > svg {
+        font-size: 4em;
+    }
+`
+
+export const Text_left = styled.div`
+    font-size: 2.4rem;
+    color: ${({theme}) => theme.COLORS.WHITE};
+`
+
+export const P_Left = styled.div`
+    font-size: 1.6rem;
+    color: ${({theme}) => theme.COLORS.GRAY};
+`
+
+export const Descricao = styled.div`
+    color: ${({theme}) => theme.COLORS.WHITE};
+    font-size: 1.3rem;
+    text-align: justify;
+    max-width: 45rem;
 `
