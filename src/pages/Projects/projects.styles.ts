62a4d1c9ea2852bba60359e4807cb91d84287f66
ArthurMartinns <arthurmartiinscontato@gmail.com>
2024-08-21 17:46:20 -0300

    Inicio da page de projects

--- src/pages/Projects/projects.styles.ts
+++ src/pages/Projects/projects.styles.ts
@@ -1,30 +1,18 @@
 import styled from "styled-components";
 
-export const Content = styled.div`
-    width: 100%;
-    border: 2px solid #000;
+export const Container = styled.div`
+    width: 100%; 
+    height: 80vh;
+    border: 1px solid ${({theme}) => theme.COLORS.WHITE};
 `
-
-export const Wrapper = styled.div`
-    align-content: center;
+export const Content = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-template-rows: 1fr 1fr;
     grid-gap: 1rem;
 
-    > div {
-        border: 2px solid #000;
-    }
-`
-
-
-export const Modal = styled.div `
-    width: 40rem;
-    height: 40rem;
-    background-color: #9d9d9d;
-    position: absolute;
-
-    > p {
-        font-size: 2rem;
+    > div{
+        border: 1px solid #fff;
+        height: 20rem;
     }
 `
--- src/pages/Projects/projects.tsx
+++ src/pages/Projects/projects.tsx
@@ -1,9 +1,18 @@
+import Container from '../../components/Container/container.view'
+import * as S from './projects.styles'
+
 function Projects() {
-
     return (
-     <>
-
-     </>
+      <Container>
+        <S.Container>
+          <S.Content>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+          </S.Content>
+        </S.Container>
+      </Container>
     )
   }
   
